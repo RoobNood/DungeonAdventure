802eb6aca4373b4c777152d6172bca917ef2ec66
RoobNood <1801100160@qq.com>
2026-05-16 17:37:21 +0800

    Add fire DoT effect support for ammo hits

---
+++ Assets/Scripts/Health/BurnDamageOverTime.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+[DisallowMultipleComponent]
+public class BurnDamageOverTime : MonoBehaviour
+{
+    private Health health;
+    private Coroutine burnCoroutine;
+
+    private void Awake()
+    {
+        health = GetComponent<Health>();
+    }
+
+    public void ApplyBurn(int damagePerTick, float tickInterval, float duration)
+    {
+        if (damagePerTick <= 0 || tickInterval <= 0f || duration <= 0f)
+            return;
+
+        if (burnCoroutine != null)
+            StopCoroutine(burnCoroutine);
+
+        burnCoroutine = StartCoroutine(BurnRoutine(damagePerTick, tickInterval, duration));
+    }
+
+    private IEnumerator BurnRoutine(int damagePerTick, float tickInterval, float duration)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            yield return new WaitForSeconds(tickInterval);
+
+            if (health != null && health.isDamageable)
+            {
+                health.TakeDamage(damagePerTick);
+            }
+
+            elapsedTime += tickInterval;
+        }
+
+        burnCoroutine = null;
+    }
+}
